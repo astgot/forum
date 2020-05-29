ea27e828820a2eddaa265a07ddee1c73c1dc0eeb
astgot <arslan.amangeldinov98@gmail.com>
2020-05-30 02:58:04 +0600

    post handling in process ...

---
+++ internal/controller/postsHandler.go
@@ -0,0 +1,10 @@
+package controller
+
+import "net/http"
+
+// PostHandler ...
+func (m *Multiplexer) PostHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
